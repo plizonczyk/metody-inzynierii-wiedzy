9bdc4cc19a68cfdc05734ebd131e54f5a31aedf8
Piotr Lizończyk <piotr.lizonczyk@gmail.com>
2016-07-01 01:06:54 +0200

    Tweak - save nodes in agds

--- agds/agds.py
+++ agds/agds.py
@@ -7,6 +7,7 @@ from .node import Node
 class AGDS(object):
     def __init__(self, data_file):
         self.root = Node('param')
+        self.id_nodes = {}
         features = []
 
         # read features and create objects
@@ -22,6 +23,7 @@ class AGDS(object):
         # fill values with id-nodes already linked
         for feature in features:
             id_node = Node(feature.feature_id)
+            self.id_nodes[feature.feature_id] = id_node
             for key, value in feature.data_dict.items():
                 self.root.get(key).add(key, Node(value), final_node=id_node)
             self.root.get('class').add(feature.feature_class, Node(feature.feature_class), final_node=id_node)
